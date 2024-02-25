fd93b1510156260e29c555e4e50adec5981e3d5d
bbbaipooo <baipoo_B@hotmail.com>
2024-02-26 02:41:39 +0700

    3000 last

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(cors({
     origin: ["https://localhost3000"],
     methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true
+    credentials: true 
+    
 }));
 
 app.use(cookieParser());
