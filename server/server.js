f6733f9a864e2c1224477d95c6f59db592e6790e
bbbaipooo <baipoo_B@hotmail.com>
2024-02-26 02:04:04 +0700

    3000

--- server/server.js
+++ server/server.js
@@ -14,6 +14,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(cors({
+    origin: ["https://localhost3000"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use(cookieParser());
 
