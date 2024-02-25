b68cc0f8c1e36e677d3a2ec13e67ab3127acf36a
bbbaipooo <baipoo_B@hotmail.com>
2024-02-26 00:51:54 +0700

    commit port

--- server/server.js
+++ server/server.js
@@ -191,6 +191,6 @@ app.delete('/deleteCat/:id', (req, res) => {
 })
 
 //port ---------------------------------
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is running on port 8800.")
 })
