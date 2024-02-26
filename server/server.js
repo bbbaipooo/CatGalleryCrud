a47202c8230e00bab97095b19eadb027aff0164b
bbbaipooo <baipoo_B@hotmail.com>
2024-02-26 17:02:08 +0700

    update

--- server/server.js
+++ server/server.js
@@ -14,12 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(cors({
-    origin: ["https://cat-gallery-crud.vercel.app"],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true 
-    
-}));
+
 
 app.use(cookieParser());
 
